1d7241b288d470acc91a34c76122caf152db9aa9
Jackson Sheppard <sheppard@slac.stanford.edu>
2020-02-18 12:20:33 -0800

    ENH: Added rms components to mirror class

--- mirror.py
+++ mirror.py
@@ -31,35 +31,40 @@ class Mirror(Device):
     couple_status_y = Component(EpicsSignalRO, ':ALREADY_COUPLED_Y_RBV', kind='normal')
     couple_status_x = Component(EpicsSignalRO, ':ALREADY_COUPLED_X_RBV', kind='normal')
 
+    # RMS Components:
+    y_enc_rms = Component(EpicsSignalRO, ':Y_ENC:RMS_RBV', kind='normal')
+    x_enc_rms = Component(EpicsSignalRO, ':X_ENC:RMS_RBV', kind='normal')
+    pitch_enc_rms = Component(EpicsSignalRO, ':PITCH_ENC:RMS_RBV', kind='normal')
+    bender_enc_rms = Component(EpicsSignalRO, ':BENDER_ENC:RMS_RBV', kind='normal')
+
     def __init__(self, prefix, **kwargs):
         super().__init__(prefix, **kwargs)
 
 
-class OMMotor(FltMvInterface, PVPositioner):
-    """
-    Base class for each motor in the FEE LCLS offset mirrors
-    """
-    __doc__ += basic_positioner_init
+#class OMMotor(FltMvInterface, PVPositioner):
+#    """
+#    Base class for each motor in the FEE LCLS offset mirrors
+#    """
+#    __doc__ += basic_positioner_init
+#
+#    # position
+#    readback = Cpt(EpicsSignalRO, '.RBV', auto_monitor=True, kind='hinted')
+#    setpoint = Cpt(EpicsSignal, '.VAL', auto_monitor=True, limits=True,
+#                   kind='normal')
+#    done = Cpt(EpicsSignalRO, '.DMOV', auto_monitor=True, kind='omitted')
+#    motor_egu = Cpt(EpicsSignal, '.EGU', kind='omitted')
+#
+#    # limit switches
+#    low_limit_switch = Cpt(EpicsSignalRO, ".LLS", kind='omitted')
+#    high_limit_switch = Cpt(EpicsSignalRO, ".HLS", kind='omitted')
 
-    # position
-    readback = Cpt(EpicsSignalRO, '.RBV', auto_monitor=True, kind='hinted')
-    setpoint = Cpt(EpicsSignal, '.VAL', auto_monitor=True, limits=True,
-                   kind='normal')
-    done = Cpt(EpicsSignalRO, '.DMOV', auto_monitor=True, kind='omitted')
-    motor_egu = Cpt(EpicsSignal, '.EGU', kind='omitted')
 
-    # limit switches
-    low_limit_switch = Cpt(EpicsSignalRO, ".LLS", kind='omitted')
-    high_limit_switch = Cpt(EpicsSignalRO, ".HLS", kind='omitted')
-
-
-# Instantiate mirror:
-#mr2l0 = Mirror('MR2L0:LFE', name='mr2l0_lfe')
+# Testing/Debugging
+mr2l0 = Mirror('MR2L0:LFE', name='mr2l0_lfe')
 
 # From the typhon README:
-#app = QApplication.instance() or QApplication(sys.argv)
-#typhon.use_stylesheet()
-#suite = typhon.TyphonSuite.from_device(mr2l0)
-#suite.show()
-#app.exec_()
-
+app = QApplication.instance() or QApplication(sys.argv)
+typhon.use_stylesheet()
+suite = typhon.TyphonSuite.from_device(mr2l0)
+suite.show()
+app.exec_()
