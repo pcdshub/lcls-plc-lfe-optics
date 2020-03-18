25f03fc7c4f2d81fc222d699f03b0a7d56dd8cba
Jackson Sheppard <sheppard@slac.stanford.edu>
2020-03-18 13:23:52 -0700

    STY: Change project names to follow convention and rebuilt ioc with ads-deploy

--- deploy_config.py
+++ deploy_config.py
@@ -1,3 +1,3 @@
-projects = ['MR1L0-MR2L0/MR1L0-MR2L0.tsproj']
+projects = ['lcls-plc-lfe-optics/lcls-plc-lfe-optics.tsproj']
 local_net_id = "172.21.42.126.1.1"
 # TODO: check local_net_id above; it should match Windows
